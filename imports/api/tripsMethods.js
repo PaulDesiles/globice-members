b77b701b6e03ed71089473f4fdcaefdf1ea88ccf
Paul Desiles <pauldesiles@gmail.com>
2021-05-07 17:44:45 +0400

    wip manual entity linking

--- imports/api/tripsMethods.js
+++ imports/api/tripsMethods.js
@@ -1,5 +1,6 @@
 import { check } from 'meteor/check';
 import { TripsCollection } from '../db/TripsCollection';
+import { MembersCollection } from '../db/MembersCollection';
 import { 
   ensureContainsUpdates, 
   ensureUserConnected,
@@ -8,14 +9,59 @@ import {
   arrayToObject 
 } from './commonMethods';
 
-function checkTrip(t) {
-  check(t.date, Date);
-  check(t.captain, String);
-  check(t.port, String);
-  check(t.type, String);
-  check(t.renter, String);
-  check(t.applicants, Array);
+
+function segregate(applicants) {
+  return {
+    selected: curApplicants.filter(a => a.assignedRole).map(a => a.memberId),
+    refused: curApplicants.filter(a => !a.assignedRole).map(a => a.memberId)
+  };
 }
+
+function getDelta(cur, prev) {
+  return {
+    add: cur.filter(n => prev.some(p => p !== n)),
+    delete: prev.filter(n => cur.some(p => p !== n)),
+  };
+}
+
+function updateMembersTripAssociation(
+  tripId,
+  currentApplicants,
+  previousApplicants)
+{
+  const current = segregate(currentApplicants);
+  const previous = segregate(previousApplicants);
+  const selectedOnes = getDelta(current.selected, previous.selected);
+  const refusedOnes = getDelta(current.refused, previous.refused);
+
+  const membersToUpdate = [
+    ...selectedOnes.add,
+    ...selectedOnes.delete,
+    ...refusedOnes.add,
+    ...refusedOnes.delete
+  ];
+
+  MembersCollection
+    .find({_id: {$in: membersToUpdate }})
+    .forEach(member => {
+      if (selectedOnes.add.some(id => id === member._id)) {
+        member.trips.confirmedTrips.push(tripId);
+      } 
+      
+      if (selectedOnes.delete.some(id => id === member._id)) {
+        member.trips.confirmedTrips.remove(tripId);
+      } 
+      
+      if (refusedOnes.add.some(id => id === member._id)) {
+        member.trips.refusedTrips.push(tripId);
+      } 
+      
+      if (refusedOnes.delete.some(id => id === member._id)) {
+        member.trips.refusedTrips.remove(tripId);
+      }
+    });
+}
+
 
 Meteor.methods({
   'trips.create'(data) {
@@ -26,10 +72,10 @@ Meteor.methods({
 
     addCreationDate(data);
 
-    var dataObj = arrayToObject(data);
-    checkTrip(dataObj);
+    const dataObj = arrayToObject(data);
+    const id = TripsCollection.insert(dataObj);
 
-    TripsCollection.insert(dataObj);
+    updateMembersTripAssociation(id, dataObj.applicants, []);
   },
 
   'trips.update'(tripId, data) {
@@ -41,8 +87,9 @@ Meteor.methods({
 
     addModificationDate(data);
 
-    var dataObj = arrayToObject(data);
+    const previousData = TripsCollection.find(tripId);
 
+    const dataObj = arrayToObject(data);
     TripsCollection.update(tripId, {
       $set: dataObj,
     });
