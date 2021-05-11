e3f650f0f5911737e897ac09caf66d23f386f6de
Paul Desiles <pauldesiles@gmail.com>
2021-05-11 17:35:20 +0400

    clean startup & init

--- imports/db/ParametersCollection.js
+++ imports/db/ParametersCollection.js
@@ -29,3 +29,36 @@ ParametersCollection.schema = new SimpleSchema({
 });
 
 ParametersCollection.attachSchema(ParametersCollection.schema);
+
+export function initParametersCollection() {    
+  const labelAsValue = (x) => ({
+    text: x,
+    value: x
+  });
+
+  ParametersCollection.insert({
+    trip: {
+      captain: [ 'Alain', 'Bernard', 'Héloïse', 'Isabelle', 'Jean-Marc', 'Lilly', 'Patrick'].map(labelAsValue),
+      type: [ 'Declic', 'Long Bec'].map(labelAsValue),
+      port: [ 'St Denis', 'St Gilles', 'St Pierre', 'Ste Rose'].map(labelAsValue),
+      renter: [ 'Batoloc', 'loc'].map(labelAsValue),
+      roles: [
+        { text:'aucun', value: null }, 
+        labelAsValue('script'), 
+        labelAsValue('observateur'), 
+        labelAsValue('photographe') 
+      ],
+    },
+    member: {
+      boatLicense: [ "Non", "Côtier", "Hauturier" ].map(labelAsValue),
+      captain: [ "Non", "Oui" ].map(labelAsValue),
+      diving: [ "Aucun", ...[1,2,3,4,5].map(x => `Niveau ${x}`) ].map(labelAsValue),
+      photo: [ "Amateur", "Amateur ++", "Professionnel" ].map(labelAsValue),
+      newMember: [ "Non", "Oui" ].map(labelAsValue),
+      bookSizeChoices: [
+        { text:'5 sorties', value: 5 }, 
+        { text:'10 sorties', value: 10 }, 
+      ]
+    },
+  });
+}
--- server/main.js
+++ server/main.js
@@ -7,64 +7,30 @@ import '/imports/api/membersMethods';
 import { TripsCollection } from '/imports/db/TripsCollection';
 import '/imports/api/tripsPublications';
 import '/imports/api/tripsMethods';
-import { ParametersCollection } from '../imports/db/ParametersCollection.js';
+import { ParametersCollection, initParametersCollection } from '../imports/db/ParametersCollection.js';
 import '/imports/api/parametersPublications';
 import '/imports/api/parametersMethods';
 
-import membersSeed from './membersSeed.js';
-
-const SEED_USERNAME = 'meteorite';
-const SEED_PASSWORD = 'password';
+// import membersSeed from './membersSeed.js';
 
 Meteor.startup(() => {
-  if (!Accounts.findUserByUsername(SEED_USERNAME)) {
-    Accounts.createUser({
-      username: SEED_USERNAME,
-      password: SEED_PASSWORD,
-    });
-  }
+  
+  // --- Helpers for seeding from a local instance ---
 
-  if (false) {
-    MembersCollection.remove({});
-    membersSeed.forEach(m => MembersCollection.insert(m));
-  }
+  // if (!Accounts.findUserByUsername('user')) {
+  //   Accounts.createUser({
+  //     username: process.env.DEFAULT_USER,
+  //     password: process.env.DEFAULT_USER_PASSWORD,
+  //   });
+  // }
 
-  if (false) {
-    TripsCollection.remove({});
-  }
+  // if (!ParametersCollection.findOne({})) {
+  //   initParametersCollection();
+  // }
 
-  var params = ParametersCollection.findOne({});
-  if (!params) {
-    
-    const labelAsValue = (x) => ({
-      text: x,
-      value: x
-    });
+  // if (true) {
+  //   MembersCollection.remove({});
+  //   membersSeed.forEach(m => MembersCollection.insert(m));
+  // }
 
-    ParametersCollection.insert({
-      trip: {
-        captain: [ 'John', 'James'].map(labelAsValue),
-        type: [ 'Declic', 'Long Bec'].map(labelAsValue),
-        port: [ 'St Denis', 'St Gilles', 'St Pierre', 'Ste Rose'].map(labelAsValue),
-        renter: [ 'Batoloc', 'loc'].map(labelAsValue),
-        roles: [
-          { text:'aucun', value: null }, 
-          labelAsValue('script'), 
-          labelAsValue('observateur'), 
-          labelAsValue('photographe') 
-        ],
-      },
-      member: {
-        boatLicense: [ "Non", "Côtier", "Hauturier" ].map(labelAsValue),
-        captain: [ "Non", "Oui" ].map(labelAsValue),
-        diving: [ "Aucun", ...[1,2,3,4,5].map(x => `Niveau ${x}`) ].map(labelAsValue),
-        photo: [ "Amateur", "Amateur ++", "Professionnel" ].map(labelAsValue),
-        newMember: [ "Non", "Oui" ].map(labelAsValue),
-        bookSizeChoices: [
-          { text:'5 sorties', value: 5 }, 
-          { text:'10 sorties', value: 10 }, 
-        ]
-      },
-    });
-  }
 });
