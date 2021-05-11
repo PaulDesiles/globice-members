205c8e262f8a820410c9d7532d1e10ae3222b104
Paul Desiles <pauldesiles@gmail.com>
2021-05-11 16:33:15 +0400

    try fix styling 2

--- imports/ui/components/MainButton.vue
+++ imports/ui/components/MainButton.vue
@@ -15,12 +15,12 @@ export default {
 </script>
 
 <style scoped>
-  .router-link a {
+  .v-application a {
     color: white;
     text-decoration: none;
   }
 
-  .router-link a img {
+  .v-application a img {
     width: 150px;
     height: 150px;
     border-radius: 50%;
@@ -30,12 +30,12 @@ export default {
     transition: border .5s, box-shadow .5s;
   }
 
-  .router-link a:hover img {
+  .v-application a:hover img {
     border-color: var(--blue-hover);
     box-shadow: 0 0 20px var(--blue);
   }
 
-  .router-link a span {
+  .v-application a span {
     display: block;
     text-align: center;
     font-size: 14pt;
